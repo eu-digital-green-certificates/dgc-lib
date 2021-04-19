db2f407dcfd6f4d46ff5dd5b7fdc90d1075dc5f2
Felix Dittrich <Felix.Dittrich@t-systems.com>
2021-04-19 13:58:37 +0200

    Added required Spring Boot Autoconfig configuration

---
+++ src/main/java/eu/europa/ec/dgc/DgcLibAutoConfiguration.java
@@ -0,0 +1,9 @@
+package eu.europa.ec.dgc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("eu.europa.ec.dgc")
+public class DgcLibAutoConfiguration {
+}
