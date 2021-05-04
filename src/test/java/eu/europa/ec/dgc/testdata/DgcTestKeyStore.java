c325e8e5e5c346f2bf583ac545fd8fc04398d54e
Felix Dittrich <Felix.Dittrich@t-systems.com>
2021-05-04 18:07:47 +0200

    Fix Jacoco config

--- src/test/java/eu/europa/ec/dgc/testdata/DgcTestKeyStore.java
+++ src/test/java/eu/europa/ec/dgc/testdata/DgcTestKeyStore.java
@@ -1,6 +1,6 @@
 /*-
  * ---license-start
- * EU Digital Green Certificate Gateway Service / dgc-gateway
+ * EU Digital Green Certificate Gateway Service / dgc-lib
  * ---
  * Copyright (C) 2021 T-Systems International GmbH and all other contributors
  * ---
